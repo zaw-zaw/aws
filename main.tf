019a103d08e8479a7df6d28936b26fac54cbd5dc
zaw-zaw <zawzawlatt169@gmail.com>
2023-03-22 20:33:03 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -46,7 +46,7 @@ data "aws_ami" "ubuntu" {
 }
 
 resource "aws_instance" "web" {
-  ami                    = data.aws_ami.ubuntu.id
+  ami                    = "ami-051f0947e420652a9"
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
 }
