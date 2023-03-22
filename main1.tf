36be55e29e702e16762cd85ce1b8f6606eb13bc2
root <root@TestVM01.gvlng0mezf3ubbaibkaq51yzaf.ix.internal.cloudapp.net>
2023-03-22 11:49:15 +0000

    version02

--- main1.tf
+++ main1.tf
@@ -34,7 +34,3 @@ resource "aws_instance" "Bastion" {
   subnet_id = "subnet-0f609bf5c2250218e"
   associate_public_ip_address = "true"
 }
-output "bastion_public_ip" {
-  description = "Public IP address of the EC2 instance"
-  value       = aws_instance.Bastion.public_ip
-}
