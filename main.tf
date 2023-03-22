a64eb5ea8c03155afa84c27fd0bc8ec44bd264c8
zaw-zaw <zawzawlatt169@gmail.com>
2023-03-22 20:31:44 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -29,12 +29,25 @@ provider "aws" {
 
 resource "random_pet" "sg" {}
 
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
 resource "aws_instance" "web" {
-  ami                    = "ami-051f0947e420652a9"
+  ami                    = data.aws_ami.ubuntu.id
   instance_type          = "t2.micro"
-#  key_name = "SingtelZZL"
- # subnet_id = "subnet-0f609bf5c2250218e"
- # associate_public_ip_address = "true"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
 }
 
