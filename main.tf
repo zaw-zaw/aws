c2da4f88aa1663235b7b6f97f0c1d22ddc52f9f7
zaw-zaw <zawzawlatt169@gmail.com>
2023-03-22 20:27:10 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -32,7 +32,7 @@ resource "random_pet" "sg" {}
 resource "aws_instance" "web" {
   ami                    = "ami-051f0947e420652a9"
   instance_type          = "t2.micro"
-  key_name = "SingtelZZL"
+#  key_name = "SingtelZZL"
   subnet_id = "subnet-0f609bf5c2250218e"
   associate_public_ip_address = "true"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
