b338ee31c56ddd4a5f7a664cbc49f2f8baa7a3ba
zaw-zaw <zawzawlatt169@gmail.com>
2023-03-22 20:13:36 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,10 +7,16 @@ terraform {
       source  = "hashicorp/aws"
       version = "4.52.0"
     }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.4.3"
+    }
   }
+  required_version = ">= 1.1.0"
 
- cloud {
+  cloud {
     organization = "1212-zzl"
+
     workspaces {
       name = "aws-git-automate"
     }
@@ -21,14 +27,56 @@ provider "aws" {
   region = "ap-southeast-1"
 }
 
-resource "aws_instance" "Bastion" {
-  ami           = "ami-051f0947e420652a9"
-  instance_type = "t2.micro"
-  key_name = "SingtelZZL"
-  subnet_id = "subnet-0f609bf5c2250218e"
-  associate_public_ip_address = "true"
+resource "random_pet" "sg" {}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
 }
-output "bastion_public_ip" {
-  description = "Public IP address of the EC2 instance"
-  value       = aws_instance.Bastion.public_ip
+
+resource "aws_instance" "web" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.web-sg.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              apt-get update
+              apt-get install -y apache2
+              sed -i -e 's/80/8080/' /etc/apache2/ports.conf
+              echo "Hello World" > /var/www/html/index.html
+              systemctl restart apache2
+              EOF
+}
+
+resource "aws_security_group" "web-sg" {
+  name = "${random_pet.sg.id}-sg"
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  // connectivity to ubuntu mirrors is required to run `apt-get update` and `apt-get install apache2`
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+output "web-address" {
+  value = "${aws_instance.web.public_dns}:8080"
 }
