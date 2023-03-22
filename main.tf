e7b1915864e8b8d212f6b7c57889b7ae82a65db8
zaw-zaw <zawzawlatt169@gmail.com>
2023-03-22 20:34:13 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -28,23 +28,6 @@ provider "aws" {
 }
 
 resource "random_pet" "sg" {}
-
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
 resource "aws_instance" "web" {
   ami                    = "ami-051f0947e420652a9"
   instance_type          = "t2.micro"
